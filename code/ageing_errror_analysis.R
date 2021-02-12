ed4e69b6700531c03915f313f072752a597f555d
Chantel Wetzel <chantel.wetzel@noaa.gov>
2021-02-12 15:04:39 -0800

    new super ageing error run

--- code/ageing_errror_analysis.R
+++ code/ageing_errror_analysis.R
@@ -116,3 +116,74 @@ for(i in 1:9)
 save(model.aic, file = file.path(dir, combo, "model_selection.dmp", sep = ""))
 
 
+########################################################
+# Ageing Error Analysis
+########################################################
+
+# Load package
+library(nwfscAgeingError)
+SourceFile <- file.path(system.file("executables", package = "nwfscAgeingError"), .Platform$file.sep)
+
+
+dir = "//nwcfile/FRAM/Assessments/CurrentAssessments/Dover_sole_2021/data/ageing_error"
+combo = 'patrick_all_cap'
+
+reads2 = read.csv(file.path(dir, "patrick_all_cap", "Complete_Modified_Age_Data.csv"))
+
+MinAge <- 1
+MaxAge <- max(ceiling(max(reads2[, 2:ncol(reads2)], na.rm = TRUE) / 10) * 10)
+
+KnotAges = list(NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA)  # Necessary for option 5 or 6
+
+BiasOpt.mat = SigOpt.mat =matrix(0,9,18)
+BiasOpt.mat[1,] =  c(0,rep(0, 17))
+BiasOpt.mat[2,] =  c(0,rep(0,17))
+BiasOpt.mat[3,] =  c(0,rep(0,17))
+BiasOpt.mat[4,] =  c(0,rep(1,17))
+BiasOpt.mat[5,] =  c(0,rep(1,17))
+BiasOpt.mat[6,] =  c(0,rep(1,17))
+BiasOpt.mat[7,] =  c(0,rep(2, 17))
+BiasOpt.mat[8,] =  c(0,rep(2, 17))
+BiasOpt.mat[9,] =  c(0,rep(2, 17))
+
+SigOpt.mat[1,] =c(1,rep(-1, 17))
+SigOpt.mat[2,] =c(2,rep(-1, 17))
+SigOpt.mat[3,] =c(3,rep(-1, 17))
+SigOpt.mat[4,] =c(1,rep(-1, 17))
+SigOpt.mat[5,] =c(2,rep(-1, 17))
+SigOpt.mat[6,] =c(3,rep(-1, 17))
+SigOpt.mat[7,] =c(1,rep(-1, 17))
+SigOpt.mat[8,] =c(2,rep(-1, 17))
+SigOpt.mat[9,] =c(3,rep(-1, 17))
+
+model.aic <- as.data.frame(matrix(NA, 9, 4))
+colnames(model.aic)<-c("Run","AIC","AICc","BIC")
+model.name<-c("B0_S1","B0_S2","B0_S3","B1_S1","B1_S2","B1_S3","B2_S1","B2_S2","B2_S3","B3_S1","B3_S2","B3_S3")
+rownames(model.aic) <- model.name[1:9]
+
+#shell("agemat.exe > output.txt 2>&1")
+
+for(i in 1:9)
+{
+  setwd(file.path(dir, combo))
+  DateFile = paste(getwd(),"/",model.name[i],"/",sep="")
+  dir.create(DateFile)
+  BiasOpt =BiasOpt.mat[i,]
+  SigOpt = SigOpt.mat[i,]
+  RunFn(Data = reads2, SigOpt = SigOpt, KnotAges = KnotAges, BiasOpt = BiasOpt,
+        NDataSets = 1, MinAge = MinAge, MaxAge = MaxAge, RefAge = 10,
+        MinusAge = 2, PlusAge = 35,
+        SaveFile = DateFile,
+        AdmbFile = SourceFile, EffSampleSize = 0, Intern = FALSE,
+        JustWrite = FALSE, CallType = "system")
+  Df = as.numeric(scan(paste(DateFile,"agemat.par",sep=""),comment.char="%", what="character", quiet=TRUE)[6])
+  Nll = as.numeric(scan(paste(DateFile,"agemat.par",sep=""),comment.char="%", what="character", quiet=TRUE)[11])
+  n = sum(ifelse(reads2[,-1]==-999,0,1))
+  Aic = 2*Nll + 2*Df
+  Aicc = Aic + 2*Df*(Df+1)/(n-Df-1)
+  Bic = 2*Nll + Df*log(n)
+  run.name<-strsplit(DateFile,"/")[[1]][3]
+  model.aic[i,]<-c(run.name, Aic, Aicc, Bic)  
+  setwd(dir)
+}
+save(model.aic, file = file.path(dir, combo, "model_selection.dmp", sep = ""))
