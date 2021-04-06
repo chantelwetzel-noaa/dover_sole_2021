03a3123c42797f28f3a8630625f5beaa2c82a47a
Chantel Wetzel <chantel.wetzel@noaa.gov>
2021-04-05 17:05:02 -0700

    edits for all model results
    includes added information in the fishery and survey data (agg data).

--- write_up/_create_doc.R
+++ write_up/_create_doc.R
@@ -8,7 +8,7 @@ devtools::load_all("C:/Users/Chantel.Wetzel/Documents/GitHub/r4ss")
 # Specify the directory for the document
 setwd("C:/Assessments/2021/dover_sole_2021/write_up")
 
-base = "//nwcfile/FRAM/Assessments/CurrentAssessments/Dover_sole_2021/models/5.7.1_dw_francis"
+base = "//nwcfile/FRAM/Assessments/CurrentAssessments/Dover_sole_2021/models/7.0.1_base"
 
 # Create the needed items to generate the "right" template that would be based on the inputs here:
 draft(authors = c("Chantel R. Wetzel", "Aaron M. Berger"),
@@ -23,9 +23,9 @@ draft(authors = c("Chantel R. Wetzel", "Aaron M. Berger"),
 # Create a model Rdata object
 read_model(
           mod_loc = base,
-				  create_plots = TRUE, 
+				  create_plots = FALSE, 
           fecund_mult = 'mt',
-          bub_scale = 12,
+          bub_scale = 4,
 				  save_loc = file.path(getwd(), "tex_tables"),
 				  verbose = TRUE)
 
