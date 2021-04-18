c6cd22919fc57cfa926d46fc8493be227da8fe2e
aaronmberger-nwfsc <1474026481@mil>
2021-04-18 07:18:44 -0700

    update catchability barplot png

--- code/surveyQ_vs_M.R
+++ code/surveyQ_vs_M.R
@@ -5,19 +5,39 @@ myQ <- data.frame(read.csv("N:/Assessments/CurrentAssessments/Dover_sole_2021/mo
 x_name <- myQ[,1]
 myQ <- as.matrix(myQ[,-1])
 rownames(myQ) <- x_name
-png("N:/Assessments/CurrentAssessments/Dover_sole_2021/models/SurveyQ_vs_M.png", width = 480, height = 350)
+
+#' @param wd location to save figure
+#' @param file plot name
+#' @param w figure width
+#' @param h figure height in inches
+#' @param pt point size
+#' @authorwritten by Chantel Wetzel
+pngfun <- function(wd, file,w=7,h=7,pt=12){
+        file <- file.path(wd, file)
+        cat('writing PNG to',file,'\n')
+        png(filename=file,
+            width=w,height=h,
+            units='in',res=300,pointsize=pt)
+}
+
+pngfun(wd = savedir, 'unavailable_biomass.png', w = 10, h = 7)
+SSunavailableSpawningOutput(model, plot=TRUE)
+dev.off()
+
+
+pngfun("N:/Assessments/CurrentAssessments/Dover_sole_2021/models","SurveyQ_vs_M.png", w = 10, h = 7)
 barplot(myQ,
         xlab = "Survey",
         ylab = "Catchability",
         col = c("green","red", "yellow","orange", "blue"),
         beside = TRUE,
-        cex.names = 0.75,
+        cex.names = 1,
 )
 legend("topleft",
        c("Base","Lorenzen","Estimate Fem.","Fix 2011 Assessment","Fix Median Prior"),
        fill = c("green","red", "yellow","orange", "blue"),
        bty="n",
-       cex = 0.7
+       cex = 1.0
 )
 abline(h=1,lty=3)
 dev.off()
