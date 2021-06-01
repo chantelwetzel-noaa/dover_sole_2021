6b8c0549f35bebbf8c7da34765ed40f0492cbf04
Chantel Wetzel <chantel.wetzel@noaa.gov>
2021-06-01 11:41:42 -0700

    fix joint profile and add new contour plotting

--- code/joint_profile_m.R
+++ code/joint_profile_m.R
@@ -6,23 +6,25 @@
 
 library(r4ss)
 mydir = "C:/Assessments/2021/dover_sole_2021/models/_sensitivities"
-base_name = "5.9.2_data_pikitch"
-base_name = "6.3.5_data_bio_no_offsets"
-base_name = "7.0.1_base_no_bio_offsets"
-base_name = "7.0.1_base_2011_survey_select"
-base_name = "7.0.1_base_2011_survey_select_no_offset"
+base_name = "7.0.1_base" ; offset = TRUE
+base_name = "7.0.1_base_no_bio_offsets"; offset = FALSE
+#base_name = "7.0.1_base_no_bio_offsets_est_m"
+base_name = "7.0.1_base_2011_survey_select_no_offset"; offset = FALSE
+base_name = "7.0.1_base_2011_survey_select"; offset = TRUE
 para = "M"
-param = c("MGparm[1]", "MGparm[12]")
-offset = FALSE
+param = c("MGparm[1]", "MGparm[13]")
+#offset = FALSE
 
 # Profile range for female M
-M_f <- seq(0.06, 0.16, 0.01)
-M_m <- seq(0.06, 0.16, 0.01)
+M_f <- seq(0.06, 0.12, 0.005)
+M_m <- seq(0.06, 0.12, 0.005)
 #M_m <- seq(-0.3, 0.3, 0.05) #seq(0.08, 0.16, 0.01)
 # Create a grid of all possible combinations
 des <- expand.grid(M_f, M_m)
 names(des) <- c("M_female", "M_male")
-des$M_offset <- log(des$M_m/des$M_f)
+if(offset){
+    des$M_offset <- log(des$M_m/des$M_f)
+} else { des$M_offset = des$M_m }
 
 # Copy base model and create folder for profile
 profile_dir = file.path(mydir, paste0(base_name, "_joint_profile_", para))
@@ -101,19 +103,76 @@ for(i in nrow(des):1) {  #start at 113 because pretty close to MLE and ss3Base.p
     file.copy("ss.par","ssstart.par", overwrite = TRUE)
 }
 
+###################################################################
+# Plot Profile Figure using ggplot
+###################################################################
+library(plotly); library(stringr); library(reshape2); 
+library(metR); library(ggplot2); library(HandyCode)
+
+mydir = "C:/Assessments/2021/dover_sole_2021/models/_sensitivities"
+mydir = "C:/Assessments/2021/dover_sole_2021/models"
+base_name = "7.0.1_base_joint_profile_M"
+base_name = "7.0.1_base_no_bio_offsets_est_m_joint_profile_M"
+base_name = "7.0.1_base_2011_survey_select_joint_profile_M"
+base_name = "7.0.1_base_2011_survey_select_no_offset_joint_profile_M"
+
+setwd(file.path(mydir,base_name))
+load("output.Rdat")
+
+out.mle <- output[nrow(output),]
+out <- as.data.frame(output[ -nrow(output), ])
+out$diffNegLogLike <- out$negLogLike - min(out$negLogLike)#out.mle["negLogLike"]
+out$diff_M <- out$M_f - out$M_m
+out <- out[order(out$M_f, out$M_m),]
+
+x <- unique(round(out$M_f, 4))
+y <- unique(round(out$M_m, 4))
+z <- matrix(out$diffNegLogLike, 
+            ncol = length(y),
+            byrow = TRUE, 
+            dimnames = list(as.character(x), as.character(y)))
+
+mtrx_melt <- melt(z, id.vars = c("M_f", "M_m"), measure.vars = 'Delta_NLL')
+names(mtrx_melt) = c("M_f", "M_m", "Delta_NLL")
+
+# Plot_ly figure
+#plot_ly(mtrx_melt, x = ~M_f, y = ~M_m, z = ~Delta_NLL, type = 'contour', 
+#        width = 600, height = 600)
+
+pngfun(wd = getwd(), file = "joint_m_profile_ggplot.png", w = 12, h = 12, pt = 12)
+ggplot(mtrx_melt, aes(x = M_f, y = M_m)) +
+    geom_contour_filled(aes(z = Delta_NLL), breaks = c(0, 2, 3, 4, 6, 10, seq(20,200, 20))) +
+    geom_text_contour(aes(z = Delta_NLL), 
+       breaks = c(2, 4, 6, seq(10, 100, 10)), size = 7, color = 'white') +
+    xlab("Natural Mortality (F)") +
+    ylab("Natural Mortality (M)") +
+    theme(
+      axis.text.y = element_text(size = 15, color = 1),
+      axis.text.x = element_text(size = 15, color = 1), 
+      axis.title.x = element_text(size = 20), 
+      axis.title.y = element_text(size = 20),
+      legend.text = element_text(size = 15), 
+      legend.title = element_text(size = 15)) +
+    guides(fill = guide_legend(title = "Change in NLL"))
+#ggsave(file.path(getwd(), "joint_m_profile_ggsave.png"), width = 12, height = 12)
+dev.off()
+
 
 ###############################################################################################
 # Plot profile
 ###############################################################################################
 
 #setwd("F:\\M\\Dover\\profileMoffset")
-#load("output.Rdat")
+#
+library(plotly)
+setwd(file.path(mydir, paste0(base_name, "_joint_profile_M")))
+load("output.Rdat")
 
 out.mle <- output[nrow(output),]
 out <- as.data.frame(output[ -nrow(output), ])
 
 out$diffNegLogLike <- out$negLogLike - out.mle["negLogLike"]
-#out$M_m <- round(out$M_f *exp(out$M_m),3)
+#out$M_m <- round(out$M_f * exp(out$M_m),3)
 out$diff_M <- out$M_f - out$M_m
 out <- out[order(out$M_f, out$M_m),]
 x <- unique(round(out$M_f, 4))
@@ -124,8 +183,53 @@ z <- matrix(out$diffNegLogLike,
             dimnames = list(as.character(x), as.character(y)))
 # Plot NLL Differences
 contour(x, y, z, 
-    levels = -5:20,
-    xlim = c(0.08, 0.12), ylim = c(0.08, 0.14),
+    levels = c(0,2,4,5,8,seq(10, 100, 10)),
+    xlim = c(0.07, 0.12), ylim = c(0.07, 0.12),
+    xlab = "Female M", ylab = "Male M")
+#points(0.108, 0.114, pch = 16, col = 'red')
+contour(x, y, z, levels = c(2), lwd = 3, add = TRUE)
+# Plot depletion
+z <- matrix(out$depletion, 
+            ncol = length(y), 
+            byrow = TRUE,
+            dimnames = list(as.character(x),as.character(y)))
+par(new = TRUE)
+contour(x, y, z, 
+        levels=c(0.25, 0.35, 0.45, 0.55, 0.65, 0.75, 0.85, 0.95),
+        method = "edge", lwd = 2, col = 'darkorchid4')
+#points(0.108, 0.108, pch = 16, col = 'red')
+z <- matrix(out$diffNegLogLike,
+            ncol = length(y),
+            byrow = TRUE, 
+            dimnames = list(as.character(x),as.character(y)))
+contour(x, y, z, levels=c(0.5,1,2,3,5,10,20,30,50,100,150),
+    xlab = "Female M", ylab = "Male M", add = TRUE, lwd = 1)
+contour(x, y, z, levels = c(3), lwd = 3, add = TRUE)
+
+
+
+
+################################################################################################
+# Plot NLL Differences
+z <- matrix(mtrx_melt$Delta_NLL, 
+            ncol = length(y),
+            byrow = TRUE, 
+            dimnames = list(as.character(x), as.character(y)))
+contour(x, y, z, 
+    levels = -5:50,
+    xlim = c(0.07, 0.12), ylim = c(0.07, 0.12),
+    xlab = "Female M", ylab = "Male M")
+points(0.108, 0.114, pch = 16, col = 'red')
+contour(x, y, z, levels = c(2), lwd = 3, add = TRUE)
+
+# Plot NLL Differences
+z <- matrix(out$diffNegLogLike, 
+            ncol = length(y),
+            byrow = TRUE, 
+            dimnames = list(as.character(x), as.character(y)))
+contour(x, y, z, 
+    levels = -5:50,
+    xlim = c(0.07, 0.12), ylim = c(0.07, 0.12),
     xlab = "Female M", ylab = "Male M")
 points(0.108, 0.114, pch = 16, col = 'red')
 contour(x, y, z, levels = c(3), lwd = 3, add = TRUE)
@@ -146,9 +250,6 @@ z <- matrix(out$diffNegLogLike,
 contour(x, y, z, levels=c(0.5,1,2,3,5,10,20,30,50,100,150),
     xlab = "Female M", ylab = "Male M", add = TRUE, lwd = 1)
 contour(x, y, z, levels = c(3), lwd = 3, add = TRUE)
-
-
-###################################################################
 
 ##############################################
 #plot Mprofile
@@ -200,9 +301,10 @@ contour(x,y,z,levels=c(0.25,0.35,0.45,0.55,0.65,0.75,0.85,0.95),method="edge",lw
 ###################################################################
 # Zoom in a bit - I can't get this figure to work
 ###################################################################
-outZoom <- out[out$diff_M < 0.021 & out$diff_M > -0.021,]
+outZoom <- out[out$diffNegLogLike <20, ]
+#outZoom <- out[out$diff_M < 0.021 & out$diff_M > -0.021,]
 x <- unique(round(outZoom$M_f, 4))
-y <- unique(round(outZoom$diff_M, 4))
+y <- unique(round(outZoom$M_m, 4))
 z <- matrix(outZoom$diffNegLogLike,
             ncol = length(y),
             byrow = TRUE,
